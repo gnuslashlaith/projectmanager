950d453d9355c7df2d8324b25e7063e00ade86ea
Laith <96651798+gnuslashlaith@users.noreply.github.com>
2025-06-09 08:40:41 +0300

    [DEM] Basic to-do list as demo

---
+++ src/ToDoList.jsx
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+
+function ToDoList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState([]);
+
+
+    function handleInput(event) {
+        setNewTask(event.target.value);
+    }
+
+    function addTask() {
+
+        if(newTask.trim() != "") {
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+        }
+
+
+    }
+
+    function deleteTask(index) {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+
+    }
+
+    return(
+        <div className="todo">
+            <h1>To-Do List</h1>
+            <h6>Get things done.</h6>
+
+            <div>
+                <input type="text" placeholder='Enter a new task.' value={newTask} onChange={handleInput}>
+
+
+                </input>
+
+                <button class="add-button" onClick={addTask}>
+                    Add 
+                </button>
+
+                
+            </div>
+
+
+            <ul>
+                {tasks.map((task, index) => 
+                    <li key={index}>
+                        <span className="text">{task}</span>
+
+                            <button class="delete-button" onClick={() => deleteTask(index)}>
+                                Remove 
+                            </button>
+
+                            <button class="markAsDone">
+                                
+                            </button>
+
+
+                    </li>
+
+                
+                
+                )} 
+            </ul>
+        
+        </div>
+        
+        );
+}
+
+export default ToDoList
