45371434fe0d63e3088ff775209ce8613307b6ce
Laith <96651798+gnuslashlaith@users.noreply.github.com>
2025-06-12 08:47:13 +0300

    Finishing up Project Dashboard UI

--- src/ProjectDashboard.jsx
+++ src/ProjectDashboard.jsx
@@ -1,41 +1,57 @@
 import React, {useState} from 'react'
 import './ProjectDashboard.css' 
 
-function Login() {
+function ProjectDashboard() {
 
 
 
     return ( 
-        <div class="sidebar">
+        <>
+            <div class="main">
+                <h1>Project Dashboard</h1>
 
-            <div class="tools">
-                <h3>Toolbar</h3>
-                <img src="logo.svg"></img>
-                <a href="https://github.com"> Project Dashboard</a>
-                <br></br>
-                <img src="logo.svg"></img>
-                <a href="https://github.com/gnuslashlaith">Calendar View</a>
-                <br></br>
-                <img src="logo.svg"></img>
-                <a href="https://github.com/gnuslashlaith">Daily Checklist</a>
+                <div class="project-list">
+                    <div class="project-card">
+                        <a href="https://github.com" class="card-name">Project Demo</a>
+                        <p class="sub">Project Manager - written in React</p>
+                    </div>
+                    <div class="project-card">
+                        <a href="https://github.com" class="card-name">Project Demo 1</a>
+                        <p class="sub">Project Manager - written in React</p>
+                    </div>
+                    <div class="project-card">
+                        <a href="https://github.com" class="card-name">Project Demo 2</a>
+                        <p>Project Manager - written in React</p>
+                    </div>
+                    <div class="project-card">
+                        <a href="https://github.com" class="card-name">Project Demo 3</a>
+                        <p class="sub">Project Manager - written in React</p>
+                    </div>
+                </div>
+
+            
             </div>
 
-            <div class="projectToolbar">
-                <h4>Projects</h4>
-                <a href="https://github.com/gnuslashlaith">Project 1</a>
-                <br></br>
-                <a href="https://github.com/gnuslashlaith">Project 2</a>
-                <br></br>
-                <a href="https://github.com"> Project 3</a>
-                <br></br>
-                <a href="https://github.com"> Project 4</a>
-                <br></br>
-                <a href="https://github.com"> Project 5</a>
-            </div>
-        </div>
+            <div class="sidebar">
+                    <div class="tools">
+                        <h3>Toolbar</h3>
+                        <a href="https://github.com"> Project Dashboard</a>
+                        
+                        <a href="https://github.com/gnuslashlaith">Calendar View</a>
+                        <a href="https://github.com/gnuslashlaith">Daily Checklist</a>
+                        <a href="https://github.com/gnuslashlaith">Pomorodo Timer</a>
+
+                        <h3>Information</h3>
+                        <a href="https://github.com/gnuslashlaith/projectmanager">Source Code</a>
+                        <a href="https://github.com/gnuslashlaith">Github Profile</a>
+                    </div>
+
+                </div>
+
+        </>
 
 
     );
 }
 
-export default Login;
+export default ProjectDashboard;
