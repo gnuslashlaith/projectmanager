0953c2fae74550ae30b0be799406d91db725b716
Laith <96651798+gnuslashlaith@users.noreply.github.com>
2025-07-02 09:00:57 +0300

    [APHA] editing UI of tasks

--- src/Components/ProjectTemplate.jsx
+++ src/Components/ProjectTemplate.jsx
@@ -93,53 +93,64 @@ export function ProjectTemplate({data}) {
             <br></br>
             <div class="task-priority-orange">
                 <textarea placeholder="What are you starting with?"  name="PriorityOrangeOne" value={goalText.PriorityOrangeOne.value}  readOnly={goalText.PriorityOrangeOne.readOnly} onChange={handleTextChange}/>
-                <button  onClick={() => setReadOnly("PriorityOrangeOne", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityOrangeOne", false)}>Edit</button>
+                <button class="read-button" onClick={() => setReadOnly("PriorityOrangeOne", true)}>Save</button>
+                <button class="read-button" onClick={() => setReadOnly("PriorityOrangeOne", false)}>Edit</button>
                 
             </div>
             <br></br>
             <div class="task-priority-blue">
                 <textarea placeholder="What's the next step to milestone 2?" name="PriorityBlueOne" value={goalText.PriorityBlueOne.value} readOnly={goalText.PriorityBlueOne.readOnly} onChange={handleTextChange}/>
-                <button placeholder="What's the next step to milestone 2?"onClick={() => setReadOnly("PriorityBlueOne", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityBlueOne", false)}>Edit</button>
+                    <button class="read-button"onClick={() => setReadOnly("PriorityBlueOne", true)}>Save</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueOne", false)}>Edit</button>
                 <br></br>
                 <textarea placeholder="What's the next step to milestone 2?"name="PriorityBlueTwo" value={goalText.PriorityBlueTwo.value} readOnly={goalText.PriorityBlueTwo.readOnly} onChange={handleTextChange}/>
-                <button onClick={() => setReadOnly("PriorityBlueTwo", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityBlueTwo", false)}>Edit</button>
+                <div class="buttons">
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueTwo", true)}>Save</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueTwo", false)}>Edit</button>
+                </div>
                 <br></br>
                 <textarea placeholder="What's the next step to milestone 2?"name="PriorityBlueThree" value={goalText.PriorityBlueThree.value} readOnly={goalText.PriorityBlueThree.readOnly} onChange={handleTextChange}/>
-                <button onClick={() => setReadOnly("PriorityBlueThree", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityBlueThree", false)}>Edit</button>
+                <div class="buttons">
+
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueThree", true)}>Save</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueThree", false)}>Edit</button>
+                </div>
             </div>
 
             <br></br>
 
             <div class="task-priority-orange">
                 <textarea placeholder="What is your halfway point?"name="PriorityOrangeTwo" value={goalText.PriorityOrangeTwo.value} readOnly={goalText.PriorityOrangeTwo.readOnly} onChange={handleTextChange}/>
-                <button onClick={() => setReadOnly("PriorityOrangeTwo", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityOrangeTwo", false)}>Edit</button>
+                <button class="read-button" onClick={() => setReadOnly("PriorityOrangeTwo", true)}>Save</button>
+                <button class="read-button" onClick={() => setReadOnly("PriorityOrangeTwo", false)}>Edit</button>
             </div>
             <br></br>
 
             <div class="task-priority-blue">
                 <textarea placeholder="final stretch! next steps to finish line." name="PriorityBlueFour" value={goalText.PriorityBlueFour.value} readOnly={goalText.PriorityBlueFour.readOnly} onChange={handleTextChange}/>
-                <button onClick={() => setReadOnly("PriorityBlueFour", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityBlueFour", false)}>Edit</button>
+                <div class="buttons">
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueFour", true)}>Save</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueFour", false)}>Edit</button>
+                </div>
                 <br></br>
                 <textarea placeholder="final stretch! next steps to finish line."name="PriorityBlueFive" value={goalText.PriorityBlueFive.value} readOnly={goalText.PriorityBlueFive.readOnly} onChange={handleTextChange}/>
-                <button onClick={() => setReadOnly("PriorityBlueFive", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityBlueFive", false)}>Edit</button>
+                <div class="buttons">
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueFive", true)}>Save</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueFive", false)}>Edit</button>
+                </div>
                 <br></br>
                 <textarea placeholder="final stretch! next steps to finish line."name="PriorityBlueSix" value={goalText.PriorityBlueSix.value} readOnly={goalText.PriorityBlueSix.readOnly}  onChange={handleTextChange}/>
-                <button onClick={() => setReadOnly("PriorityBlueSix", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityBlueSix", false)}>Edit</button>
+                <div class="buttons">
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueSix", true)}>Save</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityBlueSix", false)}>Edit</button>
+                </div>
             </div>
             <br></br>
 
             <div class="task-priority-orange">
                 <textarea placeholder="You made it! how do you know your project is done?"name="PriorityOrangeThree" value={goalText.PriorityOrangeThree.value} readOnly={goalText.PriorityOrangeThree.readOnly} onChange={handleTextChange}/>
-                <button onClick={() => setReadOnly("PriorityOrangeThree", true)}>Save</button>
-                <button onClick={() => setReadOnly("PriorityOrangeThree", false)}>Edit</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityOrangeThree", true)}>Save</button>
+                    <button class="read-button" onClick={() => setReadOnly("PriorityOrangeThree", false)}>Edit</button>
             </div>
             
 
